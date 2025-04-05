5d2124bccccd90f291bb41abb4b691d11c932307
Bhupendra Shah <shahbhupendra9211@gmail.com>
2025-04-05 18:01:15 +0545

    third commit

--- name.js
+++ name.js
@@ -5,3 +5,4 @@ console.log("my name is bhupendra");
 console.log("hello , this is git tutorial");
 console.log("line added by feature-reset branch first time");
 console.log("line added by feature-reset branch second time");
+console.log("line added by feature-reset branch third time");
