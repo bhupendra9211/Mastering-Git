42c9470982d7e7b6ab2c027d9ae9c40f89ca9b7e
Bhupendra Shah <shahbhupendra9211@gmail.com>
2025-04-08 12:24:13 +0545

    fixed the bugs

--- hello.js
+++ hello.js
@@ -3,3 +3,4 @@ console.log("this is branch 1 changes");
 console.log("dlkfjldj");
 console.log("this line is added by feature-2 branch");
 console.log("modified hello.js file by main branch");
+// urgent fixed
